206656c47781b2212fd02a49248ceb04b1385d91
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-07-15 01:49:20 +0300

    created using local storage

---
+++ src/TaskForm.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+const TaskForm = ({onAdd}) => {
+
+    const [taskName, setTaskName] = useState('');
+
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        onAdd(taskName)
+        setTaskName('')
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <button>+</button>
+            <input type="text"
+                   value={taskName}
+                   onChange={ev => setTaskName(ev.target.value)}
+                   placeholder='Your next task...'/>
+        </form>
+    );
+};
+
+export default TaskForm;
